8b26b0d21f46ee162a2f9b6e80bdf2d094df3bb8
SimutinAS <s95simutin@gmail.com>
2019-11-03 23:34:44 +0300

    try catch

--- TelegramBot/Controllers/BotController.cs
+++ TelegramBot/Controllers/BotController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NLog;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using TelegramBot.BotLogic;
+using UtilsLib;
 
 namespace TelegramBot.Controllers
 {
@@ -16,9 +18,18 @@ namespace TelegramBot.Controllers
     public class BotController : ControllerBase
     {
         [HttpPost("update")]
-        public void Update(Update request)
+        public ActionResult Update(Update request)
         {
-            Bot.Update(request);
+            try
+            {
+                var task = Bot.Update(request);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                MyLogger.Log(e.ToString(), LogLevel.Error, "BotController.Update");
+                return BadRequest();
+            }
         }
     }
 }
